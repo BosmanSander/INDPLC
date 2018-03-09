201a38838d4365dbd23346f75721249b9066bf83
Sander <15093638@student.hhs.nl>
2018-03-09 16:25:24 +0100

    Initial commit

---
+++ INDPLC/main.c
@@ -0,0 +1,18 @@
+/*
+ * INDPLC.c
+ *
+ * Created: 9-3-2018 16:12:44
+ * Author : Sander
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
